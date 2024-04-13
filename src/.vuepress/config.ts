dc21c3afaf3c0b3c6be38e2171ec5b9e9cee3c1c
Fighting <1024zzq@gmail.com>
2024-04-13 17:28:03 +0800

    Revert、加了域名

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/zzq8/",
+  base: "/",
 
   lang: "zh-CN",
   title: "Zz",
