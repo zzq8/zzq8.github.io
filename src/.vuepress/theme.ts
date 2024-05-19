9b1e7d1fbeb5804bbc3030c698821af421fd2777
Fighting <1024zzq@gmail.com>
2024-05-19 11:54:04 +0800

    1

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -55,7 +55,7 @@ export default hopeTheme({
       // appId, apiKey 和 indexName 是必填的
       appId: "ADSVTUJF43",
       apiKey: "ce680cd766327882764fa072b3b72216",
-      indexName: "zzq8"
+      indexName: "zzq8",
     },
 
     components: {
