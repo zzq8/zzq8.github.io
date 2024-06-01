b99155db070f9c9ee5e8eaa2e02541c8c4fb1ee7
MiniPC <1024zzq@gmail.com>
2024-06-02 00:10:01 +0800

    feat: todo list markdown

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,7 +1,12 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  "/daily/": "structure",
-  "/studynotes/":"structure",
-  "/resources/demo/": "structure",
+  "/": [
+    {
+      text: "参与开发",
+      children: [
+        "daily",
+      ]
+    },
+  ],
 });
